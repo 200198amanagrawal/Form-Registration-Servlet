bca2c4e3a9726070b7033bd5661e84c5f86f6118
amanagrawal <amanagrawal2098@gmail.com>
2022-04-20 01:22:30 +0530

    Cookies implemented

--- src/java/com/practice/RegisterServlet.java
+++ src/java/com/practice/RegisterServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,26 +28,16 @@ public class RegisterServlet extends HttpServlet {
         String email = req.getParameter("user_email");
         String gender = req.getParameter("user_gender");
         String course = req.getParameter("user_course");
-        String agreement = req.getParameter("user_agreement");
-        if (agreement != null) {
-            out.println("<h2>Name : " + name + "</h2>");
-            out.println("<h2>Password : " + password + "</h2>");
-            out.println("<h2>Email : " + email + "t</h2>");
-            out.println("<h2>Gender : " + gender + "</h2>");
-            out.println("<h2>Course : " + course + "</h2>");
-
-            //setting the attrubute to pass on values similar to what intent does in android
-            req.setAttribute("name", name);
-            // forwarding the data to a different servlet
-            RequestDispatcher rd = req.getRequestDispatcher("success");// url pattern of SuccessServlet
-            rd.forward(req, resp);
-        } else {
-            out.println("<h2>Agreement not checked</h2>");
-            // include the index.html content using request dispatcher
-
-            RequestDispatcher rd = req.getRequestDispatcher("index.html");
-            rd.include(req, resp);
-        }
+        out.println("<h2>Name : " + name + "</h2>");
+        out.println("<h2>Password : " + password + "</h2>");
+        out.println("<h2>Email : " + email + "t</h2>");
+        out.println("<h2>Gender : " + gender + "</h2>");
+        out.println("<h2>Course : " + course + "</h2>");
+        
+        out.println("<a href='success'>Go to Servlet 2</a>");
+        
+        Cookie cookie=new Cookie("name",name);
+        resp.addCookie(cookie);
     }
 
 }
--- src/java/com/practice/SuccessServlet.java
+++ src/java/com/practice/SuccessServlet.java
@@ -3,6 +3,7 @@ package com.practice;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,10 +16,24 @@ public class SuccessServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getDataFromCookie(resp, req);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getDataFromCookie(resp, req);
+    }
+
+    
+    private void getDataFromCookie(HttpServletResponse resp, HttpServletRequest req) throws IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        String name = (String) req.getAttribute("name");
-        out.println("<h1>Data successfully added by user " + name + "</h1>");
+        Cookie[] cookies = req.getCookies();
+        for (Cookie c : cookies) {
+            if (c.getName().equals("name")) {
+                out.println("<h1>Data successfully added by user " + c.getValue() + "</h1>");
+            }
+        }
     }
 
 }
